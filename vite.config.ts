3c1717ac311a9b585fa0e902b4602fe2e7f9801e
Lony Stewart <ext.lmejia@universal.com.do>
2026-02-17 15:42:39 -0400

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/elegant-law-landing/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
